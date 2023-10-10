1885fc95336cb9c5a6ccde2b04e98c69f4cbb942
Laurin Wolf <laurin@l5w.de>
2023-10-10 22:43:43 +0200

    highlight events in calendar widget

--- src/lib/components/widget/widgets/calendar-widget.svelte
+++ src/lib/components/widget/widgets/calendar-widget.svelte
@@ -7,6 +7,10 @@
 	type CalendarItem = {
 		name: string;
 		date: Dayjs;
+		future: boolean;
+		past: boolean;
+		today: boolean;
+		soon: boolean;
 	};
 
 	export let options: Options;
@@ -28,18 +32,23 @@
 				});
 				return properties;
 			})
-			.map((props) => {
+			.map((props): CalendarItem => {
 				const d = props['DTSTART'];
 				const year = d.substring(0, 4);
 				const month = d.substring(4, 6);
 				const day = d.substring(6, 8);
 				const hour = d.substring(9, 11);
 				const minute = d.substring(11, 13);
-				console.log(`${year}-${month}-${day} ${hour}:${minute}Z`);
+				const date = dayjs(`${year}-${month}-${day} ${hour}:${minute}Z`);
+				const diff = date.diff(dayjs(), 'hours');
 
 				return {
 					name: props['SUMMARY'].replace(/\[.*\]/, '') || 'unnamed',
-					date: dayjs(`${year}-${month}-${day} ${hour}:${minute}Z`)
+					date,
+					future: date.isAfter(dayjs(), 'day'),
+					past: date.isBefore(dayjs(), 'day'),
+					today: date.isSame(dayjs(), 'day'),
+					soon: diff <= 24 && diff >= -1
 				};
 			})
 			.sort((a, b) => a.date.unix() - b.date.unix());
@@ -57,17 +66,19 @@
 
 <div class="events">
 	{#each events as event}
-		<div class="event">
-			<strong class="name">{event.name}</strong>
-			<span>{event.date.format('HH:mm')}</span>
-			<strong>{event.date.format('DD.MM.YYYY')}</strong>
-		</div>
+		{#if !event.past}
+			<div class="event" class:soon={event.soon} class:today={event.today}>
+				<strong class="name">{event.name}</strong>
+				<span>{event.date.format('HH:mm')}</span>
+				<strong>{event.date.format('DD.MM.YYYY')}</strong>
+			</div>
+		{/if}
 	{/each}
 </div>
 
 <style lang="scss">
 	.events {
-		padding: 1rem .5rem;
+		padding: 1rem 0.5rem;
 		gap: 1rem;
 		display: flex;
 		flex-direction: column;
@@ -83,6 +94,15 @@
 
 		.name {
 			width: 100%;
+		}
+
+		&.today {
+			background: red;
+			color: #fff;
+		}
+
+		&.soon {
+			border: 4px solid red;
 		}
 	}
 </style>
