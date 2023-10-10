e7a5c3986bd767483dd8c6d2db070c66dfd74118
Laurin Wolf <laurin@l5w.de>
2023-10-10 23:25:43 +0200

    improve calendar, #10

--- src/lib/components/widget/widgets/calendar-widget.svelte
+++ src/lib/components/widget/widgets/calendar-widget.svelte
@@ -6,17 +6,28 @@
 	type Options = { url: string };
 	type CalendarItem = {
 		name: string;
-		date: Dayjs;
+		start: Dayjs;
+		end?: Dayjs;
 		future: boolean;
 		past: boolean;
 		today: boolean;
 		soon: boolean;
+		dateOnly?: boolean;
 	};
 
 	export let options: Options;
 
-	async function loadCalendar(): Promise<CalendarItem[]> {
-		const ical = await fetch(options.url).then((r) => r.text());
+	function icalToDayjs(d: string) {
+		const year = d.substring(0, 4);
+		const month = d.substring(4, 6);
+		const day = d.substring(6, 8);
+		const hour = d.substring(9, 11);
+		const minute = d.substring(11, 13);
+		return dayjs(`${year}-${month}-${day} ${hour}:${minute}Z`);
+	}
+
+	async function loadCalendar(url: string): Promise<CalendarItem[]> {
+		const ical = await fetch(url).then((r) => r.text());
 		console.log(ical);
 
 		return ical
@@ -33,31 +44,51 @@
 				return properties;
 			})
 			.map((props): CalendarItem => {
-				const d = props['DTSTART'];
-				const year = d.substring(0, 4);
-				const month = d.substring(4, 6);
-				const day = d.substring(6, 8);
-				const hour = d.substring(9, 11);
-				const minute = d.substring(11, 13);
-				const date = dayjs(`${year}-${month}-${day} ${hour}:${minute}Z`);
-				const diff = date.diff(dayjs(), 'hours');
+				const dateOnly = !!props['DTSTART;VALUE=DATE'];
+				let start;
+				let end;
+
+				if (dateOnly) {
+					start = dayjs(props['DTSTART;VALUE=DATE']);
+					if (props['DTEND;VALUE=DATE']) {
+						end = dayjs(props['DTEND;VALUE=DATE']).subtract(1, 'day');
+						if (start.isSame(end, 'day')) {
+							end = undefined;
+						}
+					}
+				} else {
+					start = icalToDayjs(props['DTSTART']);
+					if (props['DTEND']) {
+						end = icalToDayjs(props['DTEND']);
+					}
+				}
+				const diff = start.diff(dayjs(), 'hours');
 
 				return {
 					name: props['SUMMARY'].replace(/\[.*\]/, '') || 'unnamed',
-					date,
-					future: date.isAfter(dayjs(), 'day'),
-					past: date.isBefore(dayjs(), 'day'),
-					today: date.isSame(dayjs(), 'day'),
-					soon: diff <= 24 && diff >= -1
+					start,
+					end,
+					future: start.isAfter(dayjs(), 'day'),
+					past: start.isBefore(dayjs(), 'day'),
+					today: start.isSame(dayjs(), 'day'),
+					soon: diff <= 24 && diff >= -1,
+					dateOnly
 				};
-			})
-			.sort((a, b) => a.date.unix() - b.date.unix());
+			});
+	}
+
+	async function readCalendars() {
+		let events: CalendarItem[] = [];
+		const urls = options.url.split(',');
+		const calendars = await Promise.all(urls.map(async (url) => await loadCalendar(url)));
+		events = calendars.flat();
+		return events.sort((a, b) => a.start.unix() - b.start.unix());
 	}
 
 	let events: CalendarItem[] = [];
 
 	onMount(async () => {
-		events = await loadCalendar();
+		events = await readCalendars();
 		console.log(events);
 	});
 </script>
@@ -69,8 +100,13 @@
 		{#if !event.past}
 			<div class="event" class:soon={event.soon} class:today={event.today}>
 				<strong class="name">{event.name}</strong>
-				<span>{event.date.format('HH:mm')}</span>
-				<strong>{event.date.format('DD.MM.YYYY')}</strong>
+				{#if !event.dateOnly && !event.end}
+					<span>{event.start.format('HH:mm')}</span>
+				{/if}
+				<strong>{event.start.format('DD.MM.YYYY')}</strong>
+				{#if event.end}
+					- <strong>{event.end.format('DD.MM.YYYY')}</strong>
+				{/if}
 			</div>
 		{/if}
 	{/each}
@@ -85,7 +121,7 @@
 	}
 
 	.event {
-		border: 2px dashed #000;
+		outline: 2px dashed #000;
 		padding: 0.5em 1em;
 		display: flex;
 		justify-content: flex-end;
@@ -101,8 +137,9 @@
 			color: #fff;
 		}
 
-		&.soon {
-			border: 4px solid red;
+		&.soon,
+		&.today {
+			outline: 4px solid red;
 		}
 	}
 </style>
